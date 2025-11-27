7f6575b1e43fc7b690cd8d422e9f0f017993a2e3
doraxi <nguyehieu21zz@gmail.com>
2025-11-27 12:47:24 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 emotion_words = {
     "vui": ["vui", "hạnh phúc", "phấn khởi", "vui vẻ"],
-    "buồn": ["buồn", "chán", "chán nản", "thất vọng"],
+    "buồn": ["buồn","buồn bã","chán", "chán nản", "thất vọng"],
     "tức giận": ["tức", "giận", "cau có", "nổi điên"],
     "sợ hãi": ["sợ", "hoảng", "hoảng loạn"],
     "ghê tởm": ["ghê", "kinh", "ghê tởm"],
@@ -8,24 +8,33 @@ emotion_words = {
     "tin tưởng": ["tin tưởng", "yên tâm"],
     "kỳ vọng": ["mong đợi", "hi vọng", "kỳ vọng"]
 }
-negators = ["nhưng", "tuy nhiên", "mặc dù", "dù vậy"]
+negators = ["nhưng","nhưng mà","tuy nhiên", "mặc dù", "dù vậy"]
 time_words = {
     "quá_khứ": ["hôm qua", "lúc trước", "khi đó"],
-    "hiện_tại": ["bây giờ", "hiện tại", "giờ thì"]
+    "hiện_tại": ["giờ","bây giờ", "hiện tại", "giờ thì"]
 }
 
 
 def detect_emotion_rule(sentence: str):
     s= sentence.lower()
-    
+    pos=0
     for neg in negators:
-        s= s[s.find(neg)+len(neg):]
-        break
-    
+        if neg in s:
+          pos=s.find(neg)
+          s= s[pos+len(neg):]
+          break
+    s=s.strip()
+    best_emotion= None
+    best_length= 0
     for emotion, word_list in emotion_words.items():    
         for w in word_list:
-            if(w in s):                
-                return emotion
-                break 
-    
-    return "Không xác định"        
+            if(w in s):
+                if(len(w)>best_length):
+                 best_length=len(w)
+                 best_emotion=emotion    
+                   
+        
+    if best_emotion is None:
+      return "Không xác định"        
+    else: 
+       return best_emotion
